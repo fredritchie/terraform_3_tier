2127daa3e58dab7eefc89215af7276da548c944e
unknown <fredrickritchie@gmail.com>
2024-05-27 19:26:57 +0530

    first commit

---
+++ variable.tf
@@ -0,0 +1,8 @@
+variable "python_code_location" {
+  type    = string
+  default = "F:/Course/3-tier-CICD/Terraform"
+}
+variable "static_website_bucket_name" {
+  type    = string
+  default = "my-tf-hypha-ritchie-bucket"
+}
