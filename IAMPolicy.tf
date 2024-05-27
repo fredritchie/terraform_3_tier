fe16b12b2e01f892d66958a7b614a0bc8c162e8a
unknown <fredrickritchie@gmail.com>
2024-05-27 20:56:46 +0530

    first commit

--- IAMPolicy.tf
+++ IAMPolicy.tf
@@ -79,7 +79,7 @@ resource "aws_iam_policy" "policy_for_EC2" {
         "Sid": "AllowObjectDownload",
         "Effect": "Allow",
         "Action": "s3:GetObject"
-        "Resource": "arn:aws:s3:::my-tf-hypha-ritchie-bucket/frontend/*" 
+        "Resource": "arn:aws:s3:::my-tf-hypha-ritchie-bucket/*" 
       },
       {
         "Effect" : "Allow",
