2fbf627f8cc964c34665ecd5e9043c481780c2dd
unknown <fredrickritchie@gmail.com>
2024-05-27 19:28:53 +0530

    first commit

--- lambda.tf
+++ lambda.tf
@@ -1,13 +1,13 @@
-data "archive_file" "python_zip_code" {
-  type        = "zip"
-  source_file = "../backend/lambda-function.py"
-  output_path = "${path.module}lambda-function.zip"
-}
+# data "archive_file" "python_zip_code" {
+#   type        = "zip"
+#   source_file = "../backend/lambda-function.py"
+#   output_path = "${path.module}lambda-function.zip"
+# }
 
 resource "aws_lambda_function" "form" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
-  filename      = data.archive_file.python_zip_code.output_path
+  # filename      = data.archive_file.python_zip_code.output_path
   function_name = "form"
   role          = aws_iam_role.lambda_role.arn
   handler       = "lambda-function.lambda_handler"
