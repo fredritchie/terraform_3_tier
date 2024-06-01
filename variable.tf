f1857ac6e262a9a22a406373d3afa928b8c27962
unknown <fredrickritchie@gmail.com>
2024-06-01 12:13:50 +0530

    Added another s3 bucket

--- variable.tf
+++ variable.tf
@@ -3,7 +3,7 @@ variable "static_website_bucket_name" {
   type    = string
   default = "my-tf-hypha-ritchie-bucket"
 }
-variable "static_website_bucket_name" {
+variable "build-bucker-name" {
   type    = string
   default = "hypha-build-and-push-bucket"
 }
