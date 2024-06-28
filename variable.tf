ba47cff609c6963ac0b247240c6ceab6cff7b8e8
Fredrick Ritchie Eusebious Rajan <130365973+fredritchie@users.noreply.github.com>
2024-06-28 12:06:59 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,9 +1,17 @@
-
 variable "static_website_bucket_name" {
-  type    = string
-  default = "my-tf-hypha-ritchie-bucket"
+    type = string
+    default = "my-tf-static-website-${random_pet.bucket_name_suffix.id}" 
 }
+
 variable "build-bucker-name" {
-  type    = string
-  default = "hypha-build-and-push-bucket"
+    type = string
+    default = "hypha-build-${random_pet.build_bucket_name_suffix.id}"
+}
+
+resource "random_pet" "bucket_name_suffix" {
+    length = 5  # Adjust length as needed
+}
+
+resource "random_pet" "build_bucket_name_suffix" {
+    length = 5  # Adjust length as needed
 }
