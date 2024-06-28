7379e062924d1905e5b66f29f3ab22bd6121fcfe
Fredrick Ritchie Eusebious Rajan <130365973+fredritchie@users.noreply.github.com>
2024-06-28 12:13:35 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -4,10 +4,12 @@ output "alb_dns_name" {
 output "s3_website_dns_name" {
   value = aws_s3_bucket_website_configuration.static_website_bucket.website_endpoint
 }
-output "s3_bucket_name" {
+output "s3_static_website_bucket_name" {
   value = aws_s3_bucket.static_website_bucket.bucket
 }
-
+output "s3_build_bucket_name" {
+  value = aws_s3_bucket.hypha-build-and-push-bucket.bucket
+}
 output "SonarQube_IP" {
   value = aws_instance.sonarqube_vm.public_ip
 }
